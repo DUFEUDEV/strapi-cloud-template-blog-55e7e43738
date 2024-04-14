9f2577340554bd355ef6171b2c15f58b8d3538ca
Rémi Dufeu <remi.dufeu@soprasteria.com>
2024-04-14 23:21:36 +0200

    test article

--- src/api/article/services/article.js
+++ src/api/article/services/article.js
@@ -7,3 +7,17 @@
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::article.article');
+
+module.exports = {
+    async afterCreate(article) {
+      const { result } = article;
+  
+      strapi.services.notification.sendToTopic({
+        topic: 'nouvelles',
+        title: 'Nouvel Article Publié',
+        body: result.title,
+        data: { articleId: result.id },
+      });
+    }
+  };
+  
